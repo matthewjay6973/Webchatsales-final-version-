e3f59c21ed81bee18547e62572ff800035ff528c
matthew portnoy <matthewjay6973@gmail.com>
2025-08-30 19:13:34 -0400

    Update abby.js

--- api/api/abby.js
+++ api/api/abby.js
@@ -1,7 +1,1 @@
-// api/abby.js
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "✅ Abby is live and ready to update the backend!",
-    status: "online"
-  });
-} 
+
